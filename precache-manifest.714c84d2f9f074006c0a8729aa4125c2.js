33af7f1aca3381beee5513caec772f5002c60a5d
Rene <36115705+rene78@users.noreply.github.com>
2020-01-02 23:44:40 +0100

    Updates

---
+++ precache-manifest.714c84d2f9f074006c0a8729aa4125c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6e7e6e7314bf009d556",
+    "url": "/erc-721/static/js/runtime~main.b6e7e6e7.js"
+  },
+  {
+    "revision": "3059bd10c599c94e3d8f",
+    "url": "/erc-721/static/js/main.3059bd10.chunk.js"
+  },
+  {
+    "revision": "c5787be5f42b3850d0a3",
+    "url": "/erc-721/static/js/1.c5787be5.chunk.js"
+  },
+  {
+    "revision": "3059bd10c599c94e3d8f",
+    "url": "/erc-721/static/css/main.5555ee45.chunk.css"
+  },
+  {
+    "revision": "c5787be5f42b3850d0a3",
+    "url": "/erc-721/static/css/1.f3f50fc0.chunk.css"
+  },
+  {
+    "revision": "002ca596fcd63a6b0076310901859666",
+    "url": "/erc-721/index.html"
+  }
+];
